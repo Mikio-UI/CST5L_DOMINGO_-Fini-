712672c3b3c4cb49dead437497582c41b83b662c
Mikio-UI <andreimheikodomingo808@gmail.com>
2026-05-25 23:47:07 +0800

    Delete database.config.php

--- database.config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$SERVER_NAME = getenv('MYSQLHOST')     ?: 'localhost';
-$USERNAME    = getenv('MYSQLUSER')     ?: 'root';
-$PASSWORD    = getenv('MYSQLPASSWORD') ?: '';
-$DB_NAME     = getenv('MYSQLDATABASE') ?: 'railaway';
