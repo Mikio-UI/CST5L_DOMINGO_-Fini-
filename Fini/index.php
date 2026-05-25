a9856c71a7a3c9258c401a4a550fb6424d548317
Mikio-UI <andreimheikodomingo808@gmail.com>
2026-05-26 00:11:47 +0800

    Update index.php

--- Fini/index.php
+++ Fini/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 if (isset($_SESSION['user_id'])) {
-    header('Location: /Fini/dashboard.php');
+    header('Location: /dashboard.php');
 } else {
-    header('Location: /Fini/login.php');
+    header('Location: /login.php');
 }
 exit();
 ?>
